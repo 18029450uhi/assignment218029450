499824ce6ed54e48a2abaf59ee3db311b343e3fc
SefatNur <18029450@uhi.ac.uk>
2024-03-16 01:01:11 +0000

    modified file

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -9,30 +9,14 @@ import "firebase/compat/firestore";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
-var firebaseConfig = {
-  apiKey: "AIzaSyCUSuc45Y3wsARDclwbx2A-2bNp7toas_o",
-
-  authDomain: "assignment218029450.firebaseapp.com",
-
-  databaseURL:
-    "https://assignment218029450-default-rtdb.europe-west1.firebasedatabase.app",
-
-  projectId: "assignment218029450",
-
-  storageBucket: "assignment218029450.appspot.com",
-
-  messagingSenderId: "841530498032",
-
-  appId: "1:841530498032:web:c6c2cd4dc96c43be469b35",
-
-  // measurementId: "G-EQH69QFRER"
-  // apiKey: "AIzaSyD7TJWbSP3vqm90_Xan_Z11_-RSFFeSfms",
-  // authDomain: "reactapp-e6f78.firebaseapp.com",
-  // projectId: "reactapp-e6f78",
-  // storageBucket: "reactapp-e6f78.appspot.com",
-  // messagingSenderId: "877425234047",
-  // appId: "1:877425234047:web:892afe298271e929c36f4a",
-  // measurementId: "G-XFZ3VGM4T6"
+const firebaseConfig = {
+  apiKey: "AIzaSyC6CFbh2jCUz0QfLppD3ocKepsI30QWhk0",
+  authDomain: "ques-assignment.firebaseapp.com",
+  projectId: "ques-assignment",
+  storageBucket: "ques-assignment.appspot.com",
+  databaseUrl: "https://ques-assignment-default-rtdb.firebaseio.com",
+  messagingSenderId: "490894151563",
+  appId: "1:490894151563:web:17f511e61472d1f969698e"
 };
 
 // Initialize Firebase
@@ -42,7 +26,9 @@ export const auth = firebase.auth;
 export const firestore = firebase.firestore();
 export const db = firebase.database();
 
+
 const imgDB = getStorage(app);
 const textDb = getFirestore(app);
+
 
 export { imgDB, textDb };
