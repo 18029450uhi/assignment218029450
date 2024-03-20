eb81e79b7259bdb5a9317397289226f5f43de406
SefatNur <18029450@uhi.ac.uk>
2024-03-20 21:42:23 +0000

    Live Chat test update

--- src/components/LiveChatTest.js
+++ src/components/LiveChatTest.js
@@ -4,8 +4,7 @@ import firebase from "firebase/compat/app";
 const LiveChatTest = () => {
     const firestore = firebase.firestore();
     const storage = firebase.storage();
-    console.log("f",firestore);
-    console.log("s",storage);
+
     return (
         <div>
             <h1>test</h1>
