ca259452bddc93f7db05ef4fb66fc96398506f0d
SefatNur <18029450@uhi.ac.uk>
2024-05-13 02:31:21 +0100

    code cleaning

--- src/index.js
+++ src/index.js
@@ -5,20 +5,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//import firebase from 'firebase/compat/app';
-
-//import 'firebase/analytics';
-//import { BrowserRouter } from 'react-router-dom';
 
 
 
 
 
 
-// Initialize Firebase
-
-
-//firebase.getanalytics(app);
 
 
 const rootElement = 
@@ -32,7 +24,5 @@ root.render(
 );
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
